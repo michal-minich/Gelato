cfcf3c50981cb408fefaffcbe5dce88b44961e35
Michal Minich <michal.minich@gmail.com>
2012-05-16 22:37:14 +0200

    first parse

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-rdmd --build-only -m64 -debug -gs -g -w -Ilib/pegged gg.d
+# rdmd --build-only -m64 -debug -gs -g -w -Ilib/pegged gg.d
 ./gg
-rdmd --build-only -m64 -debug -gs -g -w -ofgelato -Ilib/pegged main.d
+#rdmd --build-only -m64 -debug -gs -g -w -ofgelato -Ilib/pegged main.d
+dmd -m64 -debug -gs -g -w -ofgelato main.d gel.d lib/pegged/pegged/grammar.d lib/pegged/pegged/utils/associative.d lib/pegged/pegged/peg.d
+./gelato
+rm gelato.o
--- main.d
+++ main.d
@@ -1,12 +1,12 @@
 module main;
 
-import std.stdio, std.array, std.algorithm, std.conv;
+import std.stdio, std.array, std.algorithm, std.conv, std.utf, std.file;
 import pegged.grammar;
 import gel;
 
 int main (string[] argv)
 {
-    auto parseTree1 = Gel.parse("abc");
+    auto parseTree1 = Gel.parse(toUTF32(readText!string("test.gel")));
 
     writeln(parseTree1);
 
